8637eef808beba986b1cfa742f2ded9b2a0c25ad
Bailly <jeremy.bailly@hotmail.com>
2018-12-18 18:39:02 +0100

    Add MVP Files

--- app/src/main/java/com/example/lpiem/coderproprementprojet/Views/MainActivity.java
+++ app/src/main/java/com/example/lpiem/coderproprementprojet/Views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lpiem.coderproprementprojet;
+package com.example.lpiem.coderproprementprojet.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lpiem.coderproprementprojet.R;
 
 public class MainActivity extends AppCompatActivity {
 

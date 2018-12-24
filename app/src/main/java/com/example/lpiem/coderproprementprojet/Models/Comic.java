b645e04ba8cd9333198ae86b2b1d070dc36b403b
Bailly <jeremy.bailly@hotmail.com>
2018-12-24 14:20:03 +0100

    Parcours du fichier json
    - Ajout des models pour le parcours du fichier json
    - Ajout du MainPresenter pour gérer les données pour la vue MainActivity

---
+++ app/src/main/java/com/example/lpiem/coderproprementprojet/Models/Comic.java
@@ -0,0 +1,71 @@
+package com.example.lpiem.coderproprementprojet.Models;
+
+import java.util.ArrayList;
+
+public class Comic {
+
+    private int id;
+    private String title;
+    private int issueNumber;
+    private String description;
+    private String diamondCode;
+    private String date;
+    private double price;
+    private int pageCount;
+    private String image;
+    private ArrayList<Creator> creators;
+
+    public Comic(int id, String title, int issueNumber, String description, String diamondCode,
+                 String date, double price, int pageCount, String image, ArrayList creators) {
+        this.id = id;
+        this.title = title;
+        this.issueNumber = issueNumber;
+        this.description = description;
+        this.diamondCode = diamondCode;
+        this.date = date;
+        this.price = price;
+        this.pageCount = pageCount;
+        this.image = image;
+        this.creators = creators;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDiamondCode() {
+        return diamondCode;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public ArrayList getCreators() {
+        return creators;
+    }
+}
---
+++ app/src/main/java/com/example/lpiem/coderproprementprojet/Models/Creator.java
@@ -0,0 +1,11 @@
+package com.example.lpiem.coderproprementprojet.Models;
+
+public class Creator {
+    private String name;
+    private String role;
+
+    public Creator(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
